9a80e5943ffbadbd308c0b397d1ca9fda6d628b0
Marrowsed <marcus.ah@gmail.com>
2022-05-03 08:39:45 -0300

    Semana 02 - Resumo por Ano e Mês

--- receitas/views.py
+++ receitas/views.py
@@ -1,5 +1,8 @@
+from django.db.models import Sum
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets, filters
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from receitas.serializers import *
 
@@ -50,4 +53,28 @@ class DespesaAnoMesViewSet(viewsets.ModelViewSet):
         return Despesa.objects.filter(data__year=ano, data__month=mes)
 
 # Viewset do Resumo
+
+class ResumoViewSet(APIView):
+    """
+    Visualizar Resumo por Ano/Mês
+    """
+    def get(self, request, ano, mes):
+        valor_total_receitas = Receita.objects.filter(data__year=ano, data__month=mes).aggregate(
+            Sum('valor'))['valor__sum'] or 0
+        valor_total_despesas = Despesa.objects.filter(data__year=ano, data__month=mes).aggregate(
+            Sum('valor'))['valor__sum'] or 0
+        despesa_por_categoria = Despesa.objects.filter(data__year=ano, data__month=mes).values(
+            'categoria').annotate(Sum('valor'))
+        saldo_final = valor_total_receitas - valor_total_despesas
+
+        for d in despesa_por_categoria:
+            d['valor'] = d['valor__sum']
+            del d['valor__sum']
+
+        return Response({
+            'Receita/Mês': valor_total_receitas,
+            'Despesa/Mês': valor_total_despesas,
+            'Categoria/Mês': despesa_por_categoria,
+            'Saldo Final/Mês': saldo_final
+        })
 
