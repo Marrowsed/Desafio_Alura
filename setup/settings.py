c798d7174d50f45be55761e311ed2d2896859a04
Marrowsed <marcus.ah@gmail.com>
2022-05-03 10:53:16 -0300

    pagination

--- setup/settings.py
+++ setup/settings.py
@@ -133,4 +133,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     "DATE_INPUT_FORMATS": ["%d-%m-%Y"],
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
 }
